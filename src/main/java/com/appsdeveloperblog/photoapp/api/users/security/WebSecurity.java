a0460f690d90802d0b023349c546b42cc19a8c7c
Sergey Kargopolov <sergey.kargopolov@gmail.com>
2023-02-08 21:01:41 -0500

    Configured hasIpAddress() for /users API endpoint

--- src/main/java/com/appsdeveloperblog/photoapp/api/users/security/WebSecurity.java
+++ src/main/java/com/appsdeveloperblog/photoapp/api/users/security/WebSecurity.java
@@ -1,17 +1,27 @@
 package com.appsdeveloperblog.photoapp.api.users.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.expression.WebExpressionAuthorizationManager;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurity {
+	
+	private Environment environment;
+	
+	@Autowired
+	public WebSecurity(Environment environment) {
+		this.environment = environment;
+	}
 	
 	@Bean
 	protected SecurityFilterChain configure(HttpSecurity http) throws Exception {
@@ -19,7 +29,8 @@ public class WebSecurity {
 		http.csrf().disable();
 		
 		http.authorizeHttpRequests()
-		.requestMatchers(HttpMethod.POST, "/users").permitAll()
+		.requestMatchers(HttpMethod.POST, "/users").access(
+				new WebExpressionAuthorizationManager("hasIpAddress('"+environment.getProperty("gateway.ip")+"')"))
 		.requestMatchers(new AntPathRequestMatcher("/h2-console/**")).permitAll()
 		.and()
 		.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
