3a898a1968e8cbf4004a81edefb2e7ecb79c26aa
simplyi <sergey.kargopolov@gmail.com>
2024-05-29 21:36:04 -0400

    Updated to Spring Boot version 3.2.6

--- src/main/java/com/appsdeveloperblog/photoapp/api/users/security/WebSecurity.java
+++ src/main/java/com/appsdeveloperblog/photoapp/api/users/security/WebSecurity.java
@@ -1,10 +1,8 @@
 package com.appsdeveloperblog.photoapp.api.users.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -17,8 +15,7 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 public class WebSecurity {
 	
 	private Environment environment;
-	
-	@Autowired
+
 	public WebSecurity(Environment environment) {
 		this.environment = environment;
 	}
@@ -26,16 +23,16 @@ public class WebSecurity {
 	@Bean
 	protected SecurityFilterChain configure(HttpSecurity http) throws Exception {
 		
-		http.csrf().disable();
+		http.csrf((csrf) -> csrf.disable());
 		
-		http.authorizeHttpRequests()
-		.requestMatchers(HttpMethod.POST, "/users").access(
+		http.authorizeHttpRequests((authz) -> authz
+        .requestMatchers(new AntPathRequestMatcher("/users/**")).access(
 				new WebExpressionAuthorizationManager("hasIpAddress('"+environment.getProperty("gateway.ip")+"')"))
-		.requestMatchers(new AntPathRequestMatcher("/h2-console/**")).permitAll()
-		.and()
-		.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-		
-		http.headers().frameOptions().disable();
+		.requestMatchers(new AntPathRequestMatcher("/h2-console/**")).permitAll())
+        .sessionManagement((session) -> session
+        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+ 
+        http.headers((headers) -> headers.frameOptions((frameOptions) -> frameOptions.sameOrigin()));
 		
 		return http.build();
 		
